ed249f98222b8ef6b97591e634a36c06c69f2b02
Henrique Pougy <hpougy@gmail.com>
2022-08-23 12:10:02 -0300

    ferramenta para fazer queries direto no banco

---
+++ dao/query_maker.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import psycopg2
+from config import db_data
+
+def build_conn_str(user, password, host, port, 
+                       database, dbms='postgresql'):
+    
+    conn_str = f'{dbms}://{user}:{password}@{host}:{port}/{database}'
+    
+    return conn_str
+
+def conn_tear_down(query_func):
+    
+    def func(*args, **kwargs):
+        
+        conn_str = build_conn_str(**db_data)
+        conn = psycopg2.connect(conn_str)
+        cursor = conn.cursor()
+        try:
+            result = query_func(*args, cursor = cursor, **kwargs)
+        finally:
+            cursor.close()
+            conn.close()
+        
+        return result
+    
+    return func
+
+@conn_tear_down
+def make_query(query, cursor):
+    
+    cursor.execute(query)
+    
+    return cursor.fetchall()
+
+
+class QueryMaker:
+
+    def __init__(self):
+
+        self.tables = self.list_tables()
+
+    def list_tables(self):
+
+        tables = """select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';"""
+
+        rows =  make_query(tables)
+
+        return [r[0] for r in rows]
+
+    def query_estrela(self, table, limit = None):
+        
+        if limit is None:
+            query = f"""SELECT * FROM sj2234o.{table}"""
+        else:
+            query = f"""SELECT * FROM sj2234o.{table} limit {limit}"""
+        
+        return make_query(query)
+
+    def get_col_names(self, table):
+    
+        query = f"""
+            SELECT *
+            FROM information_schema.columns
+            WHERE table_schema = 'sj2234o'
+            AND table_name   = '{table}';
+            """
+        
+        rows = make_query(query)
+        
+        nom_cols = [r[3] for r in rows]
+        
+        return nom_cols
+
+    def table_as_df(self, table, limit=10):
+    
+        rows = self.query_estrela('indicador', limit)
+        columns = self.get_col_names('indicador')
+        
+        return pd.DataFrame(data = rows, columns = columns)
+
+    def __call__(self, table, limit=10):
+
+        if table not in self.tables:
+            raise ValueError(f'Table {table} must be in {self.tables}')
+
+        return self.table_as_df(table, limit)
