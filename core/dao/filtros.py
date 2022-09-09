922146fa06a1944bc02294a98da88f0c60a7d9fe
Henrique Pougy <41967884+h-pgy@users.noreply.github.com>
2022-09-09 17:46:13 -0300

    listar resultados do indicador por nivel de regiao (#3)

--- core/dao/filtros.py
+++ core/dao/filtros.py
@@ -1,5 +1,13 @@
 
+from core.models.database import SessionLocal
+import core.models.basic as basicmodels
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 def resultados_por_nivel(resultados, nivel):
     
@@ -11,6 +19,13 @@ def resultados_por_nivel(resultados, nivel):
     filtrado = [r for r in resultados
                  if r.regiao.nivel.dc_nivel_regiao==nivel]
     
+    return filtrado
+
+def resultados_por_nivel_cd(resultados, cd_nivel_regiao):
+
+    filtrado = [r for r in resultados
+                if r.regiao.nivel.cd_nivel_regiao == cd_nivel_regiao]
+
     return filtrado
 
 def periodo_resultado(r):
@@ -33,3 +48,13 @@ def nivel_regiao_resultado(r):
 
     return r.regiao.nivel.dc_nivel_regiao
 
+
+def nomes_niveis():
+
+    db_gen = get_db()
+    db = next(db_gen)
+    model = basicmodels.NivelRegiao
+    query = db.query(model)
+    niveis = query.all()
+
+    return [n.sg_nivel_regiao for n in niveis]
