8d1a91c485d398a1954f665ece5552a5382d6a04
Henrique Pougy <hpougy@gmail.com>
2022-10-10 18:43:58 -0300

    otimizacao busca regioes e niveis regionais

--- core/dao/front_end.py
+++ core/dao/front_end.py
@@ -106,13 +106,14 @@ def search_indicador_nivel_regional(db: Session, query, niveis_regionais: list):
 
     query_aux = db.query(basicmodels.Regiao)
     query_aux = query_aux.filter(basicmodels.Regiao.cd_nivel_regiao.in_(niveis_regionais))
+    query_aux = query_aux.with_entities(basicmodels.Regiao.cd_regiao)
     regioes_nivel = query_aux.all()
     regioes_nivel = [regiao.cd_regiao for regiao in regioes_nivel] 
 
     query_aux_2 = db.query(basicmodels.ResultadoIndicador)
     query_aux_2 = query_aux_2.filter(basicmodels.ResultadoIndicador.cd_regiao.in_(regioes_nivel))
+    query_aux_2 = query_aux_2.with_entities(basicmodels.ResultadoIndicador.cd_indicador)
     cd_indicadores_nivel = set([r.cd_indicador for r in query_aux_2.all()])
-
     model = basicmodels.Indicador
     query = query.filter(model.cd_indicador.in_(cd_indicadores_nivel))
 
@@ -120,11 +121,14 @@ def search_indicador_nivel_regional(db: Session, query, niveis_regionais: list):
 
 def search_indicadores_regiao(db: Session, query, regioes: list):
 
-    query_aux = db.query(basicmodels.Regiao)
-    query_aux = query_aux.filter(basicmodels.Regiao.cd_regiao.in_(regioes))
+
+    query_aux = db.query(basicmodels.ResultadoIndicador)
+    query_aux = query_aux.filter(basicmodels.ResultadoIndicador.cd_regiao.in_(regioes))
+    query_aux = query_aux.with_entities(basicmodels.ResultadoIndicador.cd_indicador)
+
     cd_indicadores_regioes = set([r.cd_indicador for r in query_aux.all()])
 
-    model = basicmodels
+    model = basicmodels.Indicador
     query = query.filter(model.cd_indicador.in_(cd_indicadores_regioes))
 
     return query
@@ -146,10 +150,11 @@ def search_indicadores(db: Session, search : schemas.SearchIndicador):
         query = search_indicador_tema(db, query, temas)
 
     niveis_regionais = search.cd_niveis_regionais
-    if niveis_regionais:
-        query = search_indicador_nivel_regional(db, query, niveis_regionais)
-
     regioes = search.cd_regioes
+    #soh busca nivel regional se nao especificar regiao
+    if niveis_regionais and not regioes:
+        query = search_indicador_nivel_regional(db, query, niveis_regionais)
+        
     if regioes:
         query = search_indicadores_regiao(db, query, regioes)
 
