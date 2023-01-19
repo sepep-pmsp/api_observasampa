9d756c4c2c3733c93b8c5f015ecd454fe6f3d51a
Henrique Pougy <hpougy@gmail.com>
2023-01-19 10:58:50 -0300

    teste timeout exagerado

--- api.py
+++ api.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from v1 import basic_app, front_end
 import uvicorn
+import asyncio
+import time
+from fastapi import FastAPI, Request, Response, HTTPException
+from fastapi.responses import JSONResponse
+from starlette.status import HTTP_504_GATEWAY_TIMEOUT
+
+from config import REQUEST_TIMEOUT_ERROR
 
 #pode colocar markdown
 description = """
@@ -24,6 +31,23 @@ app = FastAPI(openapi_url="/",
         "url": "https://www.gnu.org/licenses/agpl-3.0.en.html",
     },
     )
+
+
+REQUEST_TIMEOUT_ERROR = 0.000001
+
+#middleware to handle timeouts
+@app.middleware("http")
+async def timeout_middleware(request: Request, call_next):
+    try:
+        start_time = time.time()
+        return await asyncio.wait_for(call_next(request), timeout=REQUEST_TIMEOUT_ERROR)
+
+    except asyncio.TimeoutError:
+        process_time = time.time() - start_time
+        return JSONResponse({'detail': 'Request processing time excedeed limit',
+                             'processing_time': process_time},
+                            status_code=HTTP_504_GATEWAY_TIMEOUT)
+
 app.include_router(basic_app, prefix="/v1/basic")
 app.include_router(front_end, prefix="/v1/front_end")
 
