4cd5001686fd51fb49cbb2776127967960c532ae
hpgy <hpougy@gmail.com>
2024-08-15 21:00:03 -0300

    remove cors middleware

--- api_observasampa/api.py
+++ api_observasampa/api.py
@@ -39,13 +39,13 @@ origins = [
     "*", #libera tudo
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+#app.add_middleware(
+#    CORSMiddleware,
+#    allow_origins=origins,
+#    allow_credentials=True,
+#    allow_methods=["*"],
+#    allow_headers=["*"],
+#)
 
 #middleware to handle timeouts
 @app.middleware("http")
